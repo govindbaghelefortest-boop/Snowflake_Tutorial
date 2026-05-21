a1bf9c0511662bb9992dcd4fc590c9d3a068b9f9
govindbaghelefortest-boop <govindbaghelefortest@gmail.com>
2026-05-21 16:48:12 +0530

    Update create_tables.sql

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -2,5 +2,6 @@ CREATE OR REPLACE TABLE revenue (
     cust_id     NUMBER(38,0) NOT NULL,
     product_id  NUMBER(38,0) NOT NULL,
     qty         NUMBER(38,0) NOT NULL,
-    sales       NUMBER(12,2) NOT NULL
+    sales       NUMBER(12,2) NOT NULL,
+    revenue       NUMBER(12,2) NOT NULL
 );
